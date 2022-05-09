5be4839552ab15a6cfe9cbc9396f2fd5310ffdd2
“thapa52” <thapa.pradeepp@gmail.comgit config --global user.name thapa52>
2022-05-09 11:41:03 +0545

    added user schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,49 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required:[true, "Please enter your name"],
+    },
+
+    avatar:{
+        public_id: String,
+        url: String,
+    },
+
+    email:{
+        type:String,
+        required:[true, "Please enter an email"],
+        unique:[true, "This email already exists"],
+    },
+
+    password:{
+        type:String,
+        required:[true, "Please enter a password"],
+        minlength:[6,"Password should be more than 6 characters"],
+        select: false,
+    },
+
+    posts:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post",
+        },
+    ],
+
+    following:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"User",
+        },
+    ],
+
+    followers:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"User",
+        },
+    ],
+});
 
 module.exports = mongoose.model("User", userSchema);
