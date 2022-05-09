08f5938522afdc9cf46d2adb051095e29b70240d
“thapa52” <thapa.pradeepp@gmail.comgit config --global user.name thapa52>
2022-05-09 15:32:56 +0545

    added post controllers

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-if(process.env.NODE_ENV !== "production"){
-    require('dotenv').config({path: "backend/config/config.env"})
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({ path: "backend/config/config.env" });
 }
 
+// Using Middleware
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Importing Routes
+const postRoute = require("./routes/post");
+
+// Using Routes
+app.use("api/v1", postRoute);
 
 module.exports = app;
