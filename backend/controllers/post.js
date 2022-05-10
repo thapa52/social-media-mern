0fd1d32cd73343a0c96dd04f4d0c377c7cd68137
“thapa52” <thapa.pradeepp@gmail.comgit config --global user.name thapa52>
2022-05-10 15:00:04 +0545

    post delete function and route added

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -32,6 +32,46 @@ exports.createPost = async (req, res) => {
   }
 };
 
+exports.deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+
+    if (post.owner.toString() !== req.user._id.toString()) {
+      return res.status(401).json({
+        success: false,
+        message: "Unathorized",
+      });
+    }
+
+    await post.remove();
+
+    const user = await User.findById(req.user._id);
+
+    const index = user.posts.indexOf(req.params.id);
+
+    user.posts.splice(index, 1);
+
+    await user.save();
+
+    res.status(201).json({
+      success: true,
+      message: "Post deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 exports.likeAndUnlikePost = async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
