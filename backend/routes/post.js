2919054303cd97de9f71886e5e2d66c8fbf02faa
“thapa52” <thapa.pradeepp@gmail.comgit config --global user.name thapa52>
2022-05-23 09:55:19 +0545

    added caption update controller

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -4,6 +4,7 @@ const {
   likeAndUnlikePost,
   deletePost,
   getPostOfFollowing,
+  updateCaption,
 } = require("../controllers/post");
 const { isAuthenticated } = require("../middlewares/auth");
 
@@ -14,6 +15,7 @@ router.route("/post/upload").post(isAuthenticated, createPost);
 router
   .route("/post/:id")
   .get(isAuthenticated, likeAndUnlikePost)
+  .put(isAuthenticated, updateCaption)
   .delete(isAuthenticated, deletePost);
 
 router.route("/posts").get(isAuthenticated, getPostOfFollowing);
