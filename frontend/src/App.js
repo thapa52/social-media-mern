479ad85e684d81b8fdbc209a95abfcf579e2b429
thapa52 <thapa.pradeepp@gmail.com>
2022-07-18 15:39:54 +0545

    frontend started with react template

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        App
+      </div>
+    </Router>
+  )
+}
+
+export default App;
