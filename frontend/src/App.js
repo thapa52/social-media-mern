8559fc28f51d00d84f245d011488247da61d891c
thapa52 <thapa.pradeepp@gmail.com>
2022-07-19 12:11:56 +0545

    added header component and styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        App
+        <Header />
       </div>
     </Router>
   )
