1fcbf15e10d1b9f9914aa335ac97ce0f19aeff2e
thapa52 <thapa.pradeepp@gmail.com>
2022-07-19 14:03:10 +0545

    added login component and route and extracting login data from backend using api

---
+++ frontend/src/Actions/User.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+export const loginUser = (email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: "LoginRequest",
+    });
+
+    const { data } = await axios.post(
+      "/api/pt/login",
+      { email, password },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    dispatch({
+      type: "LoginSuccess",
+      payload: data.user,
+    });
+  } catch (error) {
+    dispatch({
+      type: "LoginFailure",
+      payload: error,
+    });
+  }
+};
+
+export const loadUser = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: "LoadUserRequest",
+    });
+
+    const { data } = await axios.get("/api/pt/me");
+
+    dispatch({
+      type: "LoadUserSuccess",
+      payload: data.user,
+    });
+  } catch (error) {
+    dispatch({
+      type: "LoadUserFailure",
+      payload: error,
+    });
+  }
+};
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
+import Login from "./Components/Login/Login";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
       </div>
     </Router>
-  )
+  );
 }
 
 export default App;
