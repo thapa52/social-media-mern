f0708d9beb6f20e21976d5b96f0c25d1b4c5a5cf
thapa52 <thapa.pradeepp@gmail.com>
2022-07-28 18:35:57 +0545

    cloudinary configuration

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
-const app = require('./app');
-const { connectDatabase } = require('./config/database');
-
+const app = require("./app");
+const { connectDatabase } = require("./config/database");
+const cloudinary = require("cloudinary");
 
 connectDatabase();
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`server connected at port ${process.env.PORT}`);
-})
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`server connected at port ${process.env.PORT}`);
+});
