3b012db145c8910cea9a518c13dd198874078c21
thapa52 <thapa.pradeepp@gmail.com>
2022-08-02 15:17:50 +0545

    update Profile created

--- frontend/src/Actions/User.js
+++ frontend/src/Actions/User.js
@@ -155,3 +155,31 @@ export const registerUser =
       });
     }
   };
+
+export const updateProfile = (name, email, avatar) => async (dispatch) => {
+  try {
+    dispatch({
+      type: "updateProfileRequest",
+    });
+
+    const { data } = await axios.put(
+      `/api/pt/update/profile`,
+      { name, email, avatar },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    dispatch({
+      type: "updateProfileSuccess",
+      payload: data.message,
+    });
+  } catch (error) {
+    dispatch({
+      type: "updateProfileFailure",
+      payload: error.response.data.message,
+    });
+  }
+};
