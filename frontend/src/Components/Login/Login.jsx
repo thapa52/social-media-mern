2115c1ac66505283ed809acc4fed31dcb459efc8
thapa52 <thapa.pradeepp@gmail.com>
2022-08-07 15:56:13 +0545

    added error useEffect

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,20 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Login.css";
 import { Typography, Button } from "@mui/material";
 import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loginUser } from "../../Actions/User";
+import { useAlert } from "react-alert";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const { error } = useSelector((state) => state.user);
+
   const dispatch = useDispatch();
+  const alert = useAlert();
 
   const loginHandler = (e) => {
     e.preventDefault();
-    
+
     dispatch(loginUser(email, password));
   };
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch({ type: "clearErrors" });
+    }
+  }, [dispatch, error, alert]);
 
   return (
     <div className="login">
